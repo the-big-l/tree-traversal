0667a33ff5b12e5516a9db905910c2b78fc2b720
Leo Salat <leo.salat@gmail.com>
2017-01-27 11:35:13 -0800

    add assign parent methods

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,12 +1,35 @@
 # PolyTree class
 class PolyTreeNode
-  attr_reader :value
-  attr_accessor :children, :parent
+  attr_reader :value, :parent
+  attr_accessor :children
 
   def initialize(value)
     @value = value
     @parent = nil
     @children = []
   end
+
+  def parent=(parent_node)
+    unless parent == parent_node
+      if parent_node.nil?
+        parent.remove_child(self)
+        @parent = nil
+      else
+        parent_node.add_child(self) unless parent_node.children.include?(self)
+        @parent.remove_child(self) unless parent.nil?
+        @parent = parent_node
+      end
+    end
+  end
+
+  def add_child(child_node)
+    children << child_node
+  end
+
+  def remove_child(child_node)
+    children.delete(child_node)
+  end
+
+  # def remove_parent
 
 end
