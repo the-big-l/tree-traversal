6483887bca1df4b8d8378fbb12164c9c7e04ed0f
Leo Salat <leo.salat@gmail.com>
2017-01-27 11:59:35 -0800

    refactor Parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -10,32 +10,9 @@ class PolyTreeNode
   end
 
   def parent=(new_parent)
-    unless parent == new_parent
-      if new_parent.nil?
-        remove_parent
-      else
-        add_parent(new_parent)
-      end
-    end
-  end
+    parent.children.delete(self) unless parent.nil?
 
-  def add_child(child_node)
-    children << child_node
-  end
-
-  def remove_child(child_node)
-    children.delete(child_node)
-  end
-
-  def remove_parent
-    parent.remove_child(self)
-    @parent = nil
-  end
-
-  def add_parent(new_parent)
-    new_parent.add_child(self) unless new_parent.children.include?(self)
-    @parent.remove_child(self) unless parent.nil?
     @parent = new_parent
+    parent.children << self unless parent.nil?
   end
-
 end
