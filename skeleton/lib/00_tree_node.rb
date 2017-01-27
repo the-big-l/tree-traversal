0d221a53102043a15d40c2a1c4ab3cdfe4701033
Leo Salat <leo.salat@gmail.com>
2017-01-27 11:00:22 -0800

    initialize PolyTreeNode class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,12 @@
+# PolyTree class
 class PolyTreeNode
+  attr_reader :value
+  attr_accessor :children, :parent
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
 
 end
